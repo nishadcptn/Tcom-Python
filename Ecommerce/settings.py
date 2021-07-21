8cccdc28d03a97b07733b2817680e1205fbb0a30
nishadcptn <cpnishadtn@gmail.com>
2021-07-21 18:35:08 +0530

    Update settings.py

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -146,3 +146,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)

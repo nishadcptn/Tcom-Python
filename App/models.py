eca41276dcbe5fb495546479faeb9665f368d922
nishadcptn <cpnishadtn@gmail.com>
2021-08-05 19:37:06 +0530

    sample

--- App/models.py
+++ App/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django_dropbox_storage.storage import DropboxStorage
+
 
 DROPBOX_STORAGE = DropboxStorage()
 
@@ -13,7 +12,7 @@ class Location(models.Model):
     latitude = models.DecimalField( max_digits=22, decimal_places=16,null = True, blank=True)
     longitude =models.DecimalField( max_digits=22, decimal_places=16,null = True, blank=True)
     upi_number = models.CharField( max_length=10,null = True, blank=True)
-    qr_code = models.ImageField( upload_to="location/",null = True, default = None,blank=True,storage=DROPBOX_STORAGE)
+    qr_code = models.ImageField( upload_to="location/",null = True, default = None,blank=True)
     class Meta:
         db_table = "Location"
 
